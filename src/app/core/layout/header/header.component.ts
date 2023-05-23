269779ac03b9659fcdbe775253fc4853d136af0e
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-05-23 22:24:19 +0500

    created header component

---
+++ src/app/core/layout/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { LogoComponent } from './logo/header-logo.component'
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    imports: [LogoComponent],
+    standalone: true,
+})
+export class HeaderComponent {
+
+}

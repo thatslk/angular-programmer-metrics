4f83e9c0dbf82e0bc417335e3356749e8df63c49
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-05-23 16:18:37 +0500

    replaced header-logo.svg w/ header-logo component

---
+++ src/app/core/layout/header/logo/header-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './header-logo.component.html',
+    selector: 'header-logo',
+    standalone: true
+})
+export class LogoComponent {
+
+}

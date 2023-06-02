d0f2689c4a2a63537abb5f4ed80238c627834319
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-02 18:25:09 +0500

    refactored header components

--- src/app/core/layout/header/logo/header-logo.component.ts
+++ src/app/core/layout/header/logo/header-logo.component.ts
@@ -3,8 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     templateUrl: './header-logo.component.html',
     selector: 'header-logo',
+    styleUrls: ['./header-logo.component.scss'],
     standalone: true
 })
 export class LogoComponent {
-
+    logoPath:string = 'assets/images/gitlab-metrics-logo.svg'
 }

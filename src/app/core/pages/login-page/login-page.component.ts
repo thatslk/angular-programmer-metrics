0adf4ea578c9844ef322768763251d2c5f0a382e
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-02 16:02:18 +0500

    implemented authorization on login page

--- src/app/core/pages/login-page/login-page.component.ts
+++ src/app/core/pages/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { takeUntil } from "rxjs/operators";
+import { Subject } from 'rxjs';
+import LoginRequest from '../../models/request-models/login-request.model';
+import { TokensService } from '../../services/tokens.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,15 +13,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
     public loginForm!: FormGroup;
+    destroy$ = new Subject<void>();
 
-    constructor() {
+    constructor(
+        private readonly loginService: LoginService
+    ) { 
         this.loginForm = new FormGroup({
             "userLogin": new FormControl('', [Validators.required]),
             "userPassword": new FormControl('', [Validators.required]),
         });
     }
 
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+      }
+
     submit() {
         console.log(this.loginForm);
+        let loginResultObservable = this.loginService.login(new LoginRequest(this.loginForm.value['userLogin'], this.loginForm.value['userPassword']))
+        loginResultObservable.pipe(takeUntil(this.destroy$)).subscribe({
+            next: () => console.log("next")
+        })
     }
 }

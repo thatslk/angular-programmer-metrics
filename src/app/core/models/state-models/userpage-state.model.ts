f1f6e95626fb4d953af7e90ef18a346a8c46dd32
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-04 20:43:13 +0500

    created userpage state models

---
+++ src/app/core/models/state-models/userpage-state.model.ts
@@ -0,0 +1,10 @@
+export interface EventBrief {
+    project_id: number,
+    commit_hex_number: string,
+    commit_count: number,
+}
+
+export interface CodeLinesStatistic {
+    sumOfAdditions: number;
+    sumOfDeletions: number;
+}

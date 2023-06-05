c730450ea2e98e9ce49497bac66377e433bde7a8
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-05 22:50:51 +0500

    implemented logout button in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { jwtTokens } from 'app/app.module';
+import { ITokensService } from 'app/services/jwt-tokens.service';
 import { LogoComponent } from './logo/header-logo.component';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
-    imports: [LogoComponent],
+    imports: [LogoComponent, CommonModule],
     standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    constructor(
+        @Inject(jwtTokens)
+        private readonly tokenService: ITokensService,
+        private readonly _router: Router
+    ) {}
+
+    isLoggedIn = () => {
+        return this.tokenService.validateTokens();
+    };
+
+    logout = () => {
+        this.tokenService.removeTokens();
+        this._router.navigate(['login']);
+    };
+}

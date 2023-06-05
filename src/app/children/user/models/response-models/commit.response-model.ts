2ad53c7e0c7f0de755fb3993ef95cab239a0c27f
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-05 23:32:00 +0500

    removed unused fields from response models

--- src/app/children/user/models/response-models/commit.response-model.ts
+++ src/app/children/user/models/response-models/commit.response-model.ts
@@ -1,27 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 interface CommitResponse {
     id: string;
-    short_id: string;
-    created_at: string;
-    parent_ids: any[];
-    title: string;
-    message: string;
-    author_name: string;
-    author_email: string;
-    authored_date: string;
-    committer_name: string;
-    committer_email: string;
-    committed_date: string;
-    trailers: Trailers;
-    web_url: string;
     stats: Stats;
-    status: any;
     project_id: number;
-    last_pipeline: any;
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Trailers {}
 
 export interface Stats {
     additions: number;
--- src/app/children/user/models/response-models/events.response-model.ts
+++ src/app/children/user/models/response-models/events.response-model.ts
@@ -4,25 +4,7 @@ export type EventsResponse = Event[];
 interface Event {
     id: number;
     project_id: number;
-    action_name: string;
-    target_id: any;
-    target_iid: any;
-    target_type: any;
-    author_id: number;
-    target_title: any;
-    created_at: string;
-    author: Author;
     push_data?: PushData;
-    author_username: string;
-}
-
-interface Author {
-    id: number;
-    username: string;
-    name: string;
-    state: string;
-    avatar_url: string;
-    web_url: string;
 }
 
 interface PushData {
@@ -33,7 +15,6 @@ interface PushData {
     commit_to: string;
     ref: string;
     commit_title: string;
-    ref_count: any;
 }
 
 export default EventsResponse;

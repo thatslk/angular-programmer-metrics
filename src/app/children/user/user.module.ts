116bdceae5c5b425a227f748850dc9cfb6e72c5c
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-05 17:31:56 +0500

    added resolver, pipe & CommonModule in user module

--- src/app/children/user/user.module.ts
+++ src/app/children/user/user.module.ts
@@ -1,6 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserPageComponent } from './components/user-layout/user-page.component';
+import { AtPrefixPipe } from './pipes/at-prefix.pipe';
+import { userResolver } from './resolvers/user-page.resolvers';
 import { UserEventsService } from './services/helper-services/user-events.service';
 import { UserCodeLines } from './services/user-code-lines.service';
 import { UserCommitsService } from './services/user-commits.service';
@@ -11,12 +14,15 @@ const routes: Routes = [
     {
         path: '',
         component: UserPageComponent,
+        resolve: {
+            userState: userResolver,
+        },
     },
 ];
 
 @NgModule({
-    declarations: [UserPageComponent],
-    imports: [RouterModule.forChild(routes)],
+    declarations: [UserPageComponent, AtPrefixPipe],
+    imports: [RouterModule.forChild(routes), CommonModule],
     providers: [
         UserLangsService,
         UserDataService,
